849ff97a6b0fd99e9fb8e023f4cb1a877ee05d10
Peter Culver <pculver722@sbcglobal.net>
2023-08-08 14:10:33 -0400

    added ApplicationModell and JobListingModel Seed Data

--- ContractorSwapSLN/ContractorSwap/Data/DataContext.cs
+++ ContractorSwapSLN/ContractorSwap/Data/DataContext.cs
@@ -16,7 +16,40 @@ namespace ContractorSwap.Data
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
+            modelBuilder.Entity<JobListingModel>().HasData(
+                new JobListingModel()
+                {
+                    Id = 1,
+                    Name = "Test Job",
+                    Date = new DateTime(2019, 05, 09),
+                    Location = "666 Elm Street, Aurora, IL",
+                    Description = "Ceiling fan installation: Master Bedroom",
+                    PosterId = 1
+                },
+                new JobListingModel()
+                {
+                    Id = 2,
+                    Name = "Secondary Job",
+                    Date = new DateTime(2023, 06, 09),
+                    Location = "1111 Hampton Hills Ct., Hamptons, CT",
+                    Description = "Install a hot tub for a Chihuaha",
+                    PosterId = 2
+                });
+            modelBuilder.Entity<ApplicationModel>().HasData(
+                new ApplicationModel()
+                {
+                    Id = 1,
+                    Bid = 750.00,
+                    JobListingId = 1,
+                    SeekerId = 2
+                },
+                new ApplicationModel()
+                {
+                    Id = 2,
+                    Bid = 5800.00,
+                    JobListingId = 2,
+                    SeekerId = 1
+                });
         }
 
     }
