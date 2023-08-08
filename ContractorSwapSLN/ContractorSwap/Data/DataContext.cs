4ef621fa3f5901acafa654f1141826cc996c1507
Nick Baesman <baesmanw@miamioh.edu>
2023-08-08 13:09:32 -0400

    Made Context

---
+++ ContractorSwapSLN/ContractorSwap/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using ContractorSwap.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContractorSwap.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<ContractorModel> Contractors { get; set; }
+        public DbSet<JobListingModel> Jobs { get; set; }
+        public DbSet<ApplicationModel> Applications { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Server=(localdb)\\mssqllocaldb;Database=ContractorSwap;Trusted_Connection=True;";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+    }
+}
