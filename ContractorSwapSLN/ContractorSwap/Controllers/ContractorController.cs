2b154d4108f4ac36381cd34608a6ff1717a0d1b3
Nick Baesman <baesmanw@miamioh.edu>
2023-08-08 13:47:17 -0400

    started controller

---
+++ ContractorSwapSLN/ContractorSwap/Controllers/ContractorController.cs
@@ -0,0 +1,92 @@
+﻿using ContractorSwap.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContractorSwap.Controllers
+{
+    public class ContractorController : Controller
+    {
+        private readonly DataContext _Context;
+        public ContractorController(DataContext context)
+        {
+            _Context = context;
+        }
+
+        public ActionResult Index()
+        {
+            return View(_Context.Contractors.ToList()); 
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            var contractor = _Context.Contractors.Where(x=> x.Id == id).Include(x=> x.JobListings).FirstOrDefault();
+            return View(contractor);
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
