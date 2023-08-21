af336334eeb0dbc57422301d07c8c0a7825822f1
Nick Baesman <baesmanw@miamioh.edu>
2023-08-21 10:36:20 -0400

    idk

--- ContractorSwapSLN/ContractorSwap/Models/JobListingModel.cs
+++ ContractorSwapSLN/ContractorSwap/Models/JobListingModel.cs
@@ -14,7 +14,41 @@ namespace ContractorSwap.Models
         public string Name { get; set; }
 
         [Required]
-        public DateTime Date { get; set; } = DateTime.Now.Date;
+        public DateTime Date { get; set; } = DateTime.Now;
+        public string DaysSince()
+        {
+            DateTime today = DateTime.Now;
+            var diff = today - Date;
+            if (diff.TotalHours < 24)
+            {
+                return ($"{diff.Hours} Hours Ago");
+            }
+            if (diff.TotalDays < 30)
+            {   if(diff.Days == 1)
+                {
+                    return ($"{diff.Days} Day Ago");
+                }
+                return ($"{diff.Days} Days Ago");
+
+            }
+            if (diff.TotalDays/7 < 31)
+            {
+                if(diff.Days/30 == 1)
+                {
+                    return ($"{diff.Days / 30} Month Ago");
+                }
+                return ($"{diff.Days/30} Months Ago");
+            }
+            if (diff.TotalDays/30 > 11)
+            {
+                if (diff.Days / 365 == 1)
+                {
+                    return ($"{diff.Days / 365} Year Ago");
+                }
+                return ($"{diff.Days / 365} Years Ago");
+            }
+            else { return ($"{diff.Days / 7} Weeks Ago"); }
+        }
 
         [Required]
         public string Location { get; set; }
