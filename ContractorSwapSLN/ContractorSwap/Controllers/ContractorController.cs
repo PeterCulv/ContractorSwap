1011904235ba1102ce5b96da0534df2eee7bf3ee
Peter Culver <pculver722@sbcglobal.net>
2023-08-23 10:19:10 -0400

    commit cleaning views mainly MyDetails Contractor

--- ContractorSwapSLN/ContractorSwap/Controllers/ContractorController.cs
+++ ContractorSwapSLN/ContractorSwap/Controllers/ContractorController.cs
@@ -218,7 +218,7 @@ namespace ContractorSwap.Controllers
                         throw;
                     }
                 }
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(MyDetails));
             }
             return View(contractorModel);
         }
